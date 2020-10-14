b7715868e13a0901de98a9289f00bfe57876f1d8
Aldrin <aldrinjunior@gmail.com>
2020-10-13 22:26:37 -0300

    feat: map-leaflet

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -1,7 +1,44 @@
 import React from 'react';
+import { FiPlus } from 'react-icons/fi';
+import { Link } from 'react-router-dom';
+import { Map, TileLayer } from 'react-leaflet';
+
+import 'leaflet/dist/leaflet.css';
+
+import mapMarkerImg from '../images/map-marker.svg';
+
+import '../styles/pages/orphanages-map.css';
 
 function OrphanagesMap() {
-  return <h1>Hello World</h1>;
+  return (
+    <div id='page-map'>
+      <aside>
+        <header>
+          <img src={mapMarkerImg} alt='Happy' />
+
+          <h2>Escolha um orfanato no mapa</h2>
+          <p>Muitas crianças estão esperando a sua visita</p>
+        </header>
+
+        <footer>
+          <strong>Canoas</strong>
+          <span>Rio Grande do Sul</span>
+        </footer>
+      </aside>
+
+      <Map
+        center={[-29.8930949, -51.1689972]}
+        zoom={15}
+        style={{ width: '100%', height: '100%' }}
+      >
+        <TileLayer url='https://a.tile.openstreetmap.org/{z}/{x}/{y}.png' />
+      </Map>
+
+      <Link to='' className='create-orphanage'>
+        <FiPlus size={32} color='#FFF' />
+      </Link>
+    </div>
+  );
 }
 
 export default OrphanagesMap;
